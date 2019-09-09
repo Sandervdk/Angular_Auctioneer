f3307fe205ea1e8fdd436ddac8101e73df037828
Wies de Graaf <wies.de.graaf@hva.nl>
2019-09-09 21:26:00 +0200

    Angular auctioneer project made
    Created default header and main page components as per instructions in the assignment

---
+++ auctioneer/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
