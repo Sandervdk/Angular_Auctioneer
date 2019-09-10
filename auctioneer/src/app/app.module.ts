91e2b7db9efb1504a8fde5c881ee02df61fef4e7
Sander van de Kamp <sander.van.de.kamp@hva.nl>
2019-09-10 23:52:47 +0200

    Header en Navbar toegevoegd
    Header en navbar gebruiken beide Bootstrap, dus moet ook geinstalleerd worden

--- auctioneer/src/app/app.module.ts
+++ auctioneer/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/mainpage/header/header.component';
 import { HomeComponent } from './components/mainpage/home/home.component';
+import { NavbarComponent } from './components/mainpage/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
