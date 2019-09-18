ec61ae46dc1464f0d205044a3988260649035985
Sander van de Kamp <sander.van.de.kamp@hva.nl>
2019-09-18 14:34:54 +0200

    Navbar Spastische reactie verkleind gefixt

--- auctioneer/src/app/components/offers/overview1.component.ts
+++ auctioneer/src/app/components/offers/overview1.component.ts
@@ -10,7 +10,7 @@ export class Overview1 implements OnInit {
     for(let i = 0; i < 8; i++) {
       this.offers.push(this.randomOffer("Amazing Offer: " + i));
     }
-  }
+  }ng
 
   randomOffer(title: string): Offer {
     let description: string = "A description";
