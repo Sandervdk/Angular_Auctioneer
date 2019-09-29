c297d63bacaf5ef2c6356e7be537b51312facdcf
Sander van de Kamp <sander.van.de.kamp@hva.nl>
2019-09-29 18:32:04 +0200

    attempt to fix my own failures

--- auctioneer/src/app/components/offers/Offers4/offers4.component.ts
+++ auctioneer/src/app/components/offers/Offers4/offers4.component.ts
@@ -13,7 +13,6 @@ export class Offers4Component implements OnInit {
   offerSelected: boolean;
   selectedOffer: Offer;
   selectedOfferCopy: Offer;
-  offerChanged: boolean;
 
   constructor(private offersService: OffersService, private router: Router, private route: ActivatedRoute) {}
 
@@ -38,51 +37,5 @@ export class Offers4Component implements OnInit {
     this.offersService.add(newOffer);
     this.selectOffer(newOffer);
   }
-
-  deleteOffer() {
-    if (this.alertUnsavedChanges()) {
-      // Removes the selected offer and hides details from view
-      let index = this.offers.indexOf(this.selectedOffer);
-      this.offersService.remove(index);
-      this.offerSelected = false;
-    }
-  }
-
-  updateOffer(offer: Offer) {
-    let index = this.offers.indexOf(this.selectedOffer);
-
-    if (index != -1) {
-      // If index of selectedOffer is found, updates the selected offer
-      this.offersService.update(index, offer);
-    } else {
-      // If index of selectedOffer is not found, adds the new offer to the array
-      this.offersService.add(offer);
-    }
-    //Reselects the offer so that an updated offer isn't immediately reflected in the left list
-    this.selectOffer(offer);
-  }
-
-  clearOffer() {
-    // Works in conjunction with updateOffer
-    if (this.alertUnsavedChanges()) {
-      this.selectOffer(null);
-    }
-  }
-
-  resetOffer() {
-    if (this.alertUnsavedChanges()) {
-      this.selectedOfferCopy = this.selectedOffer;
-      this.selectOffer(this.selectedOfferCopy);
-    }
-  }
-
-  cancelChanges() {
-    if (this.alertUnsavedChanges()) {
-      this.resetOffer();
-      this.offerSelected = false;
-    }
-  }
-
-
 
 }
