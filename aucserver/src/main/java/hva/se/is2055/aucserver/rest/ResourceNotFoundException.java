c5c4cfb5f6ac4f0eb8c54e787e85f7cef341ef0f
Wies de Graaf <wies.de.graaf@hva.nl>
2019-12-04 17:50:43 +0100

    4.2: Added HttpStatus

--- aucserver/src/main/java/hva/se/is2055/aucserver/rest/ResourceNotFoundException.java
+++ aucserver/src/main/java/hva/se/is2055/aucserver/rest/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package hva.se.is2055.aucserver.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String s) {
         super(s);
